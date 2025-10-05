9bfd96a3597a9732c095ec3039d4d60ac1997bff
Ash <33597520+Ashterism@users.noreply.github.com>
2025-10-05 10:41:46 +0400

    added some comments

--- monipi/dataman.py
+++ monipi/dataman.py
@@ -1,13 +1,19 @@
 import csv, os
 from pathlib import Path
 
+""" 
+    this is a utility class to manage the data 
+    read / write operations to csv
+"""
+
 class Dataman:
     def __init__(self):
-        self.csvpath_readings = str("data/current_readings.csv")
-        self.csvpath_avgs = str("data/current_avgs.csv")
+        self.csvpath_samples = str("data/current_samples.csv")
+        self.csvpath_sample_averages = str("data/current_sample_averages.csv")
         # if header row missing then add?
 
     def read_row(self, tbc=''):
+        # this is a placeholder method for now
         with open(self.csvpath, "r") as currentdb:
             reader = csv.reader(currentdb)   # loads csv into reader
             
@@ -23,13 +29,13 @@ class Dataman:
     
     
     def write_readings(self, t_utc, c,t,h):
-        with open(self.csvpath_readings, "a", newline='') as readings_db:
-            writer = csv.writer(readings_db)   # loads csv into reader
+        with open(self.csvpath_samples, "a", newline='') as samples_csv:
+            writer = csv.writer(samples_csv)   # loads csv into writer
             writer.writerow([t_utc, c,t,h])
 
     def write_averages(self, t_lcl, c,t,h, t_utc):
-        with open(self.csvpath_avgs, "a", newline='') as averages_db:
-            writer = csv.writer(averages_db)   # loads csv into reader
+        with open(self.csvpath_sample_averages, "a", newline='') as averages_csv:
+            writer = csv.writer(averages_csv)   # loads csv into writer
             writer.writerow([t_lcl, c,t,h, t_utc])
 
 
